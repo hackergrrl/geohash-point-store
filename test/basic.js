688f9e6ea4805098b77c9ef302ee05eaa441bc96
Stephen Whitmore <sww@eight45.net>
2017-08-12 16:48:22 -0700

    add test

---
+++ test/basic.js
@@ -0,0 +1,57 @@
+var test = require('tape')
+var level = require('memdb')
+var haversine = require('haversine')
+var Db = require('..')
+
+test('many random points', function (t) {
+  var db = Db(level())
+
+  var expected = {}
+  var at = [0.3, 0.3]
+
+  var n = 50000
+  var spread = 1
+  var pending = n
+  function insert () {
+    if (!pending) return check()
+    var x = 0.5 + Math.random() * spread - spread/2
+    var y = 0.5 + Math.random() * spread - spread/2
+
+    if (haversineDistanceKm(at, [x, y]) <= 1) {
+      var str = [x, y].join(',')
+      expected[str] = true
+    }
+
+    db.insert([x, y], function () {
+      pending--
+      insert()
+    })
+  }
+  insert()
+
+  function check () {
+    console.time('query')
+    var q = db.queryStream8(at, 1)
+    var matches = 0
+    q.on('data', function (pt) {
+      if (expected[pt.value]) matches++
+    })
+    q.once('end', function () {
+      console.timeEnd('query')
+      t.equal(Object.keys(expected).length, matches)
+      t.end()
+    })
+  }
+})
+
+function haversineDistanceKm (from, to) {
+  var from = {
+    latitude: from[0],
+    longitude: from[1]
+  }
+  var to = {
+    latitude: to[0],
+    longitude: to[1]
+  }
+  return haversine(from, to, {unit: 'meter'}) / 1000
+}
